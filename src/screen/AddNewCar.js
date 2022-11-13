8f8a0dc1764c85f8a0affee8fda341563f1f2212
pathakrahul0 <rahulpathak.ind@gmail.com>
2022-11-13 13:14:50 +0530

    Car Edit API Done

--- src/screen/AddNewCar.js
+++ src/screen/AddNewCar.js
@@ -1,12 +1,26 @@
 import React, { useState } from 'react';
 import {
-    Image, FlatList, Text, View,
-    StyleSheet, TouchableOpacity, SafeAreaView, TextInput, ScrollView,
+    Image,
+    FlatList,
+    Text,
+    View, Keyboard,
+    StyleSheet,
+    TouchableOpacity,
+    SafeAreaView,
+    TextInput,
+    ScrollView,
 } from 'react-native';
 import * as Colors from '../utils/Colors';
 import AdaptiveStatusBar from '../component/AdaptiveStatusBar';
 import Loader from '../component/Loader';
 import CommonAppBar from '../component/CommonAppBar';
+import NetInfo from "@react-native-community/netinfo";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Toast from 'react-native-simple-toast';
+import Constants from '../utils/Constants';
+import Links from '../utils/Links';
+import Utils from '../utils/Utils';
+import LoaderView from '../component/LoaderView'
 
 
 export default class AddNewCar extends React.Component {
@@ -15,21 +29,141 @@ export default class AddNewCar extends React.Component {
         this.state = {
             isNetworkAvailable: true,
             isLoading: false,
-            oldPassword: "",
-            newPassword: "",
-            confirmNewPassword: "",
+            deviceType: "1",
+            carNo: "car-1234",
+            model: "TataV29878",
+            fuleType: "Diesel",
+            make: "Tata1",
+            isHybrid: 0,
+            status: 0,
+            year: "2011",
+            regoExpireDate: "30/11/2023",
+            insuranceExpireDate: "30/10/2023",
+            carImage: null,
+            carId: "1",
             isHybridChecked: false,
             isCarStatusChecked: false,
         }
+    }
+
+
+    componentDidMount = async () => {
+        this.userId = await AsyncStorage.getItem(Constants.STORAGE_KEY_USER_ID);
+        this.apiKey = await AsyncStorage.getItem(Constants.STORAGE_KEY_API_KEY);
     }
 
     onClickSubmitButton() {
 
     }
 
+    callAddNewCarValidation() {
+        Keyboard.dismiss();
+        if (this.state.carNo == "") {
+            Toast.show("Please enter Car No", Toast.SHORT);
+        }
+        else if (this.state.model == "") {
+            Toast.show("Please enter model", Toast.SHORT);
+        }
+        else if (this.state.fuleType == "") {
+            Toast.show("Please enter Fule Type", Toast.SHORT);
+        }
+        else if (this.state.make == "") {
+            Toast.show("Please enter make", Toast.SHORT);
+        }
+        else if (this.state.year == "") {
+            Toast.show("Please enter Year", Toast.SHORT);
+        }
+        else if (this.state.regoExpireDate == "") {
+            Toast.show("Please enter Rego Expire Date", Toast.SHORT);
+        }
+        else if (this.state.insuranceExpireDate == "") {
+            Toast.show("Please enter Insurance Expire Date", Toast.SHORT);
+        }
+        // else if (this.state.carImage == null) {
+        //   Toast.show("Please enter Car Image", Toast.SHORT);
+        // }
+        else {
+            try {
+                NetInfo.fetch().then(state => {
+                    if (state.isConnected) {
+                        this.callAddNewCarApi();
+                    }
+                    else {
+                        Utils.showMessageAlert("No internet connection")
+                    }
+                });
+            }
+            catch (error) {
+                console.log("Error in webservice call : " + error);
+            }
+        }
+    }
+
+    callAddNewCarApi = async () => {
+        this.setState({ isLoading: true });
+
+        var formData = new FormData();
+        formData.append('token_key', this.apiKey);
+        formData.append('device_type', this.state.deviceType);
+        formData.append('user_id', this.state.userId);
+        formData.append('car_no', this.state.carNo);
+        formData.append('make', this.state.make);
+        formData.append('model', this.state.model);
+        formData.append('year', this.state.year);
+        formData.append('fuel_type', this.state.fuleType);
+        formData.append('is_hybrid', this.state.isHybrid);
+        formData.append('status', this.state.status);
+        formData.append('rego_expire_date', this.state.regoExpireDate);
+        formData.append('insurance_expire_date', this.state.insuranceExpireDate);
+        formData.append('car_image', this.state.carImage);
+        formData.append('car_id', this.state.carId);
+
+
+
+        try {
+            console.log("Call Car Edit API ========>  ", JSON.stringify(formData));
+            const res = await fetch(Links.EDIT_CAR, {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    Accept: "application/json",
+                    //   'Content-Type': 'application/json',
+                    "Content-Type": "multipart/form-data",
+                },
+            });
+            const responseJSON = await res.json();
+            console.log("Car Edit Response ===========>  ", JSON.stringify(responseJSON));
+            if (responseJSON) {
+                if (responseJSON.hasOwnProperty("status") && responseJSON.status == 1) {
+                    this.setState({ isLoading: false });
+                    if (responseJSON.hasOwnProperty("message") && responseJSON.message) {
+                        Toast.show(responseJSON.message, Toast.SHORT);
+                    }
+                    this.props.navigation.goBack()
+
+                }
+                else if (responseJSON.hasOwnProperty("status") && responseJSON.status == 0) {
+                    this.setState({ isLoading: false });
+                    if (responseJSON.hasOwnProperty("message") && responseJSON.message) {
+                        Toast.show(responseJSON.message, Toast.SHORT);
+                    } else {
+                        Toast.show("something went wrong", Toast.SHORT);
+                    }
+                }
+            }
+        }
+        catch (error) {
+            this.setState({ isLoading: false });
+            Toast.show("something went wrong", Toast.SHORT);
+            console.log("Exception in API call: " + error);
+        }
+
+    }
+
     render() {
         return (
             <SafeAreaView style={styles.container}>
+                {this.state.isLoading && <LoaderView />}
                 <CommonAppBar title="Add New Car" navigation={this.props.navigation} />
 
                 <ScrollView >
@@ -266,7 +400,7 @@ export default class AddNewCar extends React.Component {
                                 marginEnd: 10,
                                 flex: 1,
                             }}
-                                onPress={() => this.onClickSubmitButton()}>
+                                onPress={() => this.callAddNewCarValidation()}>
                                 <Text numberOfLines={1} style={styles.buttonText}>SUBMIT</Text>
                             </TouchableOpacity>
 
