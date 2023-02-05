985643e14913f23fc98432328619f49f9cf0cc7e
Karan Singh <karankumarsngh@gmail.com>
2023-02-05 11:49:01 +0530

    Edit car fixes

--- src/screen/AddNewCar.js
+++ src/screen/AddNewCar.js
@@ -139,9 +139,9 @@ export default class AddNewCar extends React.Component {
             regoExpireDate: this.state.item.rego_expire_date,
             insuranceExpireDate: this.state.item.insurance_expire_date,
             imageUri: Links.BASEURL + this.state.item.car_image,
-            isHybridYes: this.state.item.is_hybrid === "Yes" ? true : false,
-            isHybridNo: this.state.item.is_hybrid === "No" ? true : false,
-            isHybridNo: this.state.item.is_hybrid === "" ? true : false,
+            isHybridYes: this.state.item.is_hybrid === "1" ? true : false,
+            isHybridNo: this.state.item.is_hybrid === "0" ? true : false,
+            //isHybridNo: this.state.item.is_hybrid === "" ? true : false,
             isCarStatusActive: this.state.item.status === "1" ? true : false,
             isCarStatusInactive: this.state.item.status === "0" ? true : false,
             // com: item.company_id,
@@ -358,8 +358,8 @@ export default class AddNewCar extends React.Component {
         formData.append('model', this.state.model);
         formData.append('year', this.state.year);
         formData.append('fuel_type', this.state.fuleType);
-        formData.append('is_hybrid', this.state.isHybridYes);
-        formData.append('status', this.state.isCarStatusActive);
+        formData.append('is_hybrid', this.state.isHybridYes === true ? "1" : "0");
+        formData.append('status', this.state.isCarStatusActive === true ? "1" : "0");
         formData.append('rego_expire_date', this.state.regoExpireDate);
         formData.append('insurance_expire_date', this.state.insuranceExpireDate);
         if(this.state.imageName.trim().length>0){
